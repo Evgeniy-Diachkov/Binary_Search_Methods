68accc9e24b5ac8ba18a416c4e46e3c166dae93f
Evgeny <j-z-b@ya.ru>
2024-11-28 00:00:53 +0300

    Second commit

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -1,15 +1,76 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Comparator;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class MyArrays {
+
+    // Для массивов byte
+    public static int binarySearch(byte[] a, byte key) {
+        return java.util.Arrays.binarySearch(a, key);
+    }
+
+    public static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key);
+    }
+
+    // Для массивов char
+    public static int binarySearch(char[] a, char key) {
+        return java.util.Arrays.binarySearch(a, key);
+    }
+
+    public static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key);
+    }
+
+    // Для массивов double
+    public static int binarySearch(double[] a, double key) {
+        return java.util.Arrays.binarySearch(a, key);
+    }
+
+    public static int binarySearch(double[] a, int fromIndex, int toIndex, double key) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key);
+    }
+
+    // Для массивов float
+    public static int binarySearch(float[] a, float key) {
+        return java.util.Arrays.binarySearch(a, key);
+    }
+
+    public static int binarySearch(float[] a, int fromIndex, int toIndex, float key) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key);
+    }
+
+    // Для массивов int
+    public static int binarySearch(int[] a, int key) {
+        return java.util.Arrays.binarySearch(a, key);
+    }
+
+    public static int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key);
+    }
+
+    // Для массивов long
+    public static int binarySearch(long[] a, long key) {
+        return java.util.Arrays.binarySearch(a, key);
+    }
+
+    public static int binarySearch(long[] a, int fromIndex, int toIndex, long key) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key);
+    }
+
+    // Для массивов short
+    public static int binarySearch(short[] a, short key) {
+        return java.util.Arrays.binarySearch(a, key);
+    }
+
+    public static int binarySearch(short[] a, int fromIndex, int toIndex, short key) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key);
+    }
+
+    // Для массивов объектов с Comparator
+    public static <T> int binarySearch(T[] a, T key, Comparator<? super T> c) {
+        return java.util.Arrays.binarySearch(a, key, c);
+    }
+
+    public static <T> int binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c) {
+        return java.util.Arrays.binarySearch(a, fromIndex, toIndex, key, c);
     }
 }
--- src/MyCollections.java
+++ src/MyCollections.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
+import java.util.List;
 
 public class MyCollections {
+
+    // Для List с естественным порядком
+    public static <T extends Comparable<? super T>> int binarySearch(List<? extends T> list, T key) {
+        return java.util.Collections.binarySearch(list, key);
+    }
+
+    // Для List с пользовательским Comparator
+    public static <T> int binarySearch(List<? extends T> list, T key, Comparator<? super T> c) {
+        return java.util.Collections.binarySearch(list, key, c);
+    }
 }
+
